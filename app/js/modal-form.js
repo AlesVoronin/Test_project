088d0300abe27cc665623f2f0f8deb98381858aa
AlesVoronin <alesworonin@gmail.com>
2021-08-26 01:33:40 +0300

    feat: added desktop version site

---
+++ app/js/modal-form.js
@@ -0,0 +1,8 @@
+const open_button = document.querySelector(".pokemon-form-button")
+const form = document.querySelector(".modal-form");
+const wrapper = document.querySelector(".basic-wrapper");
+
+const openForm = () => {
+
+}
+
